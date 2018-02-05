b65ab346f4457c06c4d48cb010fff3c2a507f094
David Roller <droller47@gmail.com>
2018-02-04 19:03:11 -0500

    ADDED: added a new elb test

--- tests.py
+++ tests.py
@@ -41,6 +41,17 @@ class WebserverTest(unittest.TestCase):
         r = requests.get("https://{0}".format(url), allow_redirects=False, verify=False)
         assert "<h1>Hello World</h1>" in r.text
 
+    def test_only_80_and_443_open_on_elb(self):
+        client = boto3.client('elb')
+        elb_data = client.describe_load_balancers(LoadBalancerNames=['webserver-elb'])
+        listeners = elb_data['LoadBalancerDescriptions'][0]['ListenerDescriptions']
+        ports = set()
+        for listener in listeners:
+            ports.add(listener['Listener']['LoadBalancerPort'])
+            ports.add(listener['Listener']['InstancePort'])
+        sorted_ports = sorted(ports)
+        self.assertEqual(sorted_ports, [80,443])
+
 
 if __name__=='__main__':
     unittest.main()
